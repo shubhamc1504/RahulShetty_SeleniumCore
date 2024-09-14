c621a7a8ac9d643fd967ab3b873d2d2c39168e6c
shubhamc1504 <shubhamc1504@gmail.com>
2024-09-14 10:59:35 +0530

    Final commit

--- src/test/java/Locators.java
+++ src/test/java/Locators.java
@@ -4,36 +4,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 public class Locators {
+    public static void main(String[] args) throws InterruptedException {
 
-
-
-public static void main(String[] args) throws InterruptedException {
-
-// TODO Auto-generated method stub
-
-//implicit wait - 2 seconds time out
-WebDriver driver = new ChromeDriver();
-driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-driver.get("https://rahulshettyacademy.com/locatorspractice/");
-driver.findElement(By.id("inputUsername")).sendKeys("Shubham");
-driver.findElement(By.name("inputPassword")).sendKeys("hello123");
-driver.findElement(By.className("signInBtn")).click();
-System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
-driver.findElement(By.linkText("Forgot your password?")).click();
-Thread.sleep(1000);//
-driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("Shubham");
-driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("shubham@gmail.com");
-driver.findElement(By.xpath("//input[@type='text'][2]")).clear();
-driver.findElement(By.cssSelector("input[type='text']:nth-child(3)")).sendKeys("shubham@gmail.com");
-driver.findElement(By.xpath("//form/input[3]")).sendKeys("8286448862");
-driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
-System.out.println(driver.findElement(By.cssSelector("form p")).getText());
-driver.findElement(By.xpath("//div[@class='forgot-pwd-btn-conainer']/button[1]")).click();
-Thread.sleep(1000);
-driver.findElement(By.cssSelector("#inputUsername")).sendKeys("Shubham");
-driver.findElement(By.cssSelector("input[type*='pass']")).sendKeys("rahulshettyacademy");
-driver.findElement(By.id("chkboxOne")).click();
-driver.findElement(By.xpath("//button[contains(@class,'submit')]")).click();
+        WebDriver driver = new ChromeDriver();
+        //Implicit wait
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        Thread.sleep(3000);
+    }
 }
-}
-
--- src/test/java/Locators2.java
+++ src/test/java/Locators2.java
@@ -4,24 +4,39 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 
 import java.time.Duration;
+import java.util.Arrays;
 
 import static org.openqa.selenium.By.*;
 
 public class Locators2 {
     public static void main(String[] args) throws InterruptedException {
-        String name= "Shubham";
+        String name = "Shubham";
         WebDriver driver = new ChromeDriver(); //Webdriver Interface creates driver object using new keyword and Chromedriver constructor
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        String password = Arrays.toString(getPassword(driver));
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
         driver.findElement(id("inputUsername")).sendKeys(name);
-        driver.findElement(name("inputPassword")).sendKeys("rahulshettyacademy");
+        driver.findElement(name("inputPassword")).sendKeys(password);
         driver.findElement(className("signInBtn")).click();
         Thread.sleep(2000);
         System.out.println(driver.findElement(By.tagName("p")).getText());
         //Compare actual value with expected result using Assert Class
         Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
-        Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(),"Hello "+name+",");
+        Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(), "Hello " + name + ",");
         driver.findElement(By.xpath("//button[text()='Log Out']")).click();
         driver.close();
+    }
+
+    public static String[] getPassword(WebDriver driver) throws InterruptedException {
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.findElement(By.linkText("Forgot your password?")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+        String passwordText = driver.findElement(By.cssSelector("form p")).getText();
+        //Please use temporary password 'rahulshettyacademy' to Login.
+        String[] passwordArray = passwordText.split("'"); //rahulshettyacademy' to Login.
+        //String[] passwordArray2 = passwordArray[1].split("'");
+        //passwordArray2[0];
+        return passwordArray[1].split("'");
     }
 }
