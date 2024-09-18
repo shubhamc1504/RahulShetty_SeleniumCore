cd143a1f45bd482bcfe1a7d7cbe164a0b5c8c390
shubhamc1504 <shubhamc1504@gmail.com>
2024-09-18 21:25:26 +0530

    Final commit

--- src/test/java/Dropdowns_Static_Passengers.java
+++ src/test/java/Dropdowns_Static_Passengers.java
@@ -12,7 +12,11 @@ public class Dropdowns_Static_Passengers {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://www.spicejet.com/"); //URL in browser
         driver.findElement(By.cssSelector("div[class='css-1dbjc4n'] div div[class='css-1dbjc4n r-14lw9ot r-11u4nky r-z2wwpe r-1phboty r-rs99b7 r-1loqt21 r-13awgt0 r-ymttw5 r-5njf8e r-1otgn73'] div[class='css-1dbjc4n'] svg")).click(); //Dropdown option click
-        driver.findElement(By.xpath("(//*[name()='path'][@fill-rule='nonzero'])[10]")).click(); //Adding 1 adult into the passengers
+        int i=0;
+        while (i<4) {
+            driver.findElement(By.xpath("(//*[name()='path'][@fill-rule='nonzero'])[10]")).click(); //Adding 3 adults into the passengers; 4 adults in total
+            i++;
+        }
         driver.findElement(By.xpath("(//div[@class='css-1dbjc4n r-1awozwy r-19m6qjp r-z2wwpe r-1loqt21 r-18u37iz r-1777fci r-d9fdf6 r-1w50u8q r-ah5dr5 r-1otgn73'])[1]")).click(); //Click on done button
 
     }
