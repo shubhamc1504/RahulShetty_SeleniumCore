b6859a74bb79b7bf58abd0da9aed575e6b951a3e
shubhamc1504 <shubhamc1504@gmail.com>
2024-10-15 08:44:18 +0530

    Initial Commit

--- src/test/java/WebTableSorting.java
+++ src/test/java/WebTableSorting.java
@@ -8,9 +8,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class WebTableSorting {
-    public static void main(String[] args)
-    {
-        WebDriver driver= new ChromeDriver();
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
 
@@ -21,13 +20,25 @@ public class WebTableSorting {
         List<WebElement> veggies = driver.findElements(By.xpath("//tr/td[1]"));
 
         //capture text of all elements into new list
-        List<String> originalVeggiesList= veggies.stream().map(s->s.getText()).collect(Collectors.toList());
+        List<String> originalVeggiesList = veggies.stream().map(s -> s.getText()).collect(Collectors.toList());
 
         //apply sorting of list ->sorted original list
-        List<String> sortedList  =originalVeggiesList.stream().sorted().collect(Collectors.toList());
+        List<String> sortedList = originalVeggiesList.stream().sorted().collect(Collectors.toList());
 
         //compare original list with sorted list
         Assert.assertEquals(sortedList, originalVeggiesList);
 
+        //scan the name column with getText ->Beans and print the price of it
+        List<String> price = veggies.stream()
+                .filter(s -> s.getText().contains("Beans"))
+                .map(s -> getPriceVeggies(s))
+                .collect(Collectors.toList());
+        price.forEach(a->System.out.println(a));
     }
+
+    private static String getPriceVeggies(WebElement s) {
+        String priceval = s.findElement(By.xpath("following-sibling::td[1]")).getText();
+        return priceval;
+    }
+
 }
